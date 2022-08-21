c033fa587d48fc5b3647c4d2f881bd2fa9729f9a
nicolas2289h <91564340+nicolas2289h@users.noreply.github.com>
2022-08-21 17:47:42 -0300

    -

--- main.js
+++
@@ -1,8 +0,0 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
-}
-
-menuEmail.addEventListener('click', toggleDesktopMenu)
